16a138603be7fe874bfeea2ea8c4a0ffce477078
jessoliva <95152563+jessoliva@users.noreply.github.com>
2022-03-13 23:25:53 -0500

    added queries to display single candidate, delete a candidate, and create a candidate

--- server.js
+++ server.js
@@ -30,15 +30,47 @@ const db = mysql.createConnection(
     console.log('Connected to the election database.')
 );
 
+// GET all candidates
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+// });
+// returns an array of objects, with each object representing a row of the candidates table
 // the db object is using the query() method
 // method runs the SQL query and executes the callback with all the resulting rows that match the query
 // Once this method executes the SQL command, the callback function captures the responses from the query in two variables: the err, which is the error response, and rows, which is the database query response
 // If there are no errors in the SQL query, the err value is null
 // This method is the key component that allows SQL commands to be written in a Node.js application.
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
-});
-// returns an array of objects, with each object representing a row of the candidates table
+
+// GET a single candidate
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log(row);
+// });
+// selecting a single candidate by their primary key (id) is the only way to ensure that the candidate requested is the one that's returned
+
+// Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log(result);
+// });
+// The DELETE statement has a question mark (?) that denotes a placeholder, making this a prepared statement. A prepared statement can execute the same SQL statements repeatedly using different values in place of the placeholder
+// An additional param argument following the prepared statement provides values to use in place of the prepared statement's placeholders
+
+// Create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+// //
+// db.query(sql, params, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
+
 
 // order of routes matter so this route should be at the very end since it's a catchall
 // Default response for any other request (Not Found)
