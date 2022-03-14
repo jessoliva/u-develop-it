84584b00ea5fa6af672995e2cf07c0a263cddc47
jessoliva <95152563+jessoliva@users.noreply.github.com>
2022-03-13 23:09:14 -0500

    used the query method to execute SQL command to display all rows from candidates table

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+// import mysql2 package
+const mysql = require('mysql2');
+
 // to use Express.js
 const express = require('express');
 const app = express();
@@ -14,8 +17,28 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 // need these middleware for POST and PUT requests because in both these requests you are sending data (in the form of some data object) to the server and you are asking the server to accept or store that data (object), which is enclosed in the body (i.e. req.body) of that (POST or PUT) Request
 
-// what is displayed on root page
+// connects the application to the MySQL database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'pek3jed1',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
+// the db object is using the query() method
+// method runs the SQL query and executes the callback with all the resulting rows that match the query
+// Once this method executes the SQL command, the callback function captures the responses from the query in two variables: the err, which is the error response, and rows, which is the database query response
+// If there are no errors in the SQL query, the err value is null
+// This method is the key component that allows SQL commands to be written in a Node.js application.
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
+// returns an array of objects, with each object representing a row of the candidates table
 
 // order of routes matter so this route should be at the very end since it's a catchall
 // Default response for any other request (Not Found)
