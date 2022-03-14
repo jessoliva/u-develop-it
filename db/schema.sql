d0b5a36f1f7ac1f18358944d01649c5b2a115dfe
jessoliva <95152563+jessoliva@users.noreply.github.com>
2022-03-14 15:34:44 -0500

    created parties table

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,12 @@
 -- by running source db/schema.sql, it runs this command and creates the table!
 
+CREATE TABLE parties (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  description TEXT
+);
+-- TEXT data type used for description because VARCHAR data type must declare a limit on the length
+
 CREATE TABLE candidates (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
