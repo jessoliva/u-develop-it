d100a24eeb9e814ed28e483ba6e6bfb87a3f3559
jessoliva <95152563+jessoliva@users.noreply.github.com>
2022-03-14 19:09:47 -0500

    added voters table and data; used DATETIME, DEFAULT, CURRENT_TIMESTAMP

--- db/schema.sql
+++ db/schema.sql
@@ -3,6 +3,7 @@
 DROP TABLE IF EXISTS candidates;
 DROP TABLE IF EXISTS parties;
 -- As noted previously, the order of table creation is vital due to the dependency of the candidates table on the existence of a parties.id. In the same regard, the candidates table must be dropped before the parties table due to the foreign key constraint that requires the parties table to exist.
+DROP TABLE IF EXISTS voters;
 
 CREATE TABLE parties (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
@@ -23,3 +24,15 @@ CREATE TABLE candidates (
 -- Because this constraint relies on the parties table, the parties table MUST be defined first before the candidates table
 -- Because we've established a strict rule that no candidate can be a member of a party that doesn't exist, we should also consider what should happen if a party is deleted --> we added ON DELETE SET NULL to tell SQL to set a candidate's party_id field to NULL if the corresponding row in parties is ever deleted.
 
+CREATE TABLE voters (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+--  we added a new field, created_at, with a data type of DATETIME. In SQL, a DATETIME field's value will look something like 2020-01-01 13:00:00
+-- That probably doesn't seem very user-friendly, but the front-end team can take that value, convert it with JavaScript's Date() constructor, and display anything they want at that point
+-- DEFAULT: If you don't specify NOT NULL, then a field could potentially be NULL if that value isn't provided in an INSERT statement. With DEFAULT, however, you can specify what the value should be if no value is provided
+-- CURRENT_TIMESTAMP: This will return the current date and time in the same 2020-01-01 13:00:00 format. Note that the time will be based on what time it is according to your server, not the client's machine
+-- So, in our code we're specifying CURRENT_TIMESTAMP as the value for DEFAULT
